8bb4cddb79a2c68853a351834a9d4fb1c64da8f3
jiyujia0417 <jiyujia0417@163.com>
2020-11-01 15:08:40 +0800

    值引入redux

--- ReduxPages/App.js
+++ ReduxPages/App.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react'
-import todolist from './reducers'
 import store from './store'
+// provider 提供整个状态的应用树
+import {Provider} from 'react-redux'
+
+// Redux：提供可预测的状态管理的容器
+// 解决跨层级传递属性的问题
 
 const App = () => {
     const [todos, setTodos] = useState(store.getState())
@@ -12,7 +16,7 @@ const App = () => {
     }, [])
 
     return <div>
-        {todos.map(item => <li>{item}</li>)}
+        {todos.map((item, index) => <li key={index}>{item}</li>)}
         <button onClick={() => {
             store.dispatch({
                 type: 'ADD_TODO',
